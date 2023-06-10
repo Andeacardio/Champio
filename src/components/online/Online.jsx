4f930a4023e2e001c4f3e6b4a4b96e418e5e0be4
Andeacardio <107910228+Andeacardio@users.noreply.github.com>
2023-06-10 19:23:39 +0300

    Update Online.jsx

--- src/components/online/Online.jsx
+++ src/components/online/Online.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./online.css";
+import { Link } from "react-router-dom";
 
 export default function Online({ user }) {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
@@ -7,15 +8,17 @@ export default function Online({ user }) {
     <div>
       <li className="rightbarFriend">
         <div className="ridhtbarImgContainer">
-          <img
-            className="rightbarProfileImg"
-            src={
-              user.profilePicture
-                ? PF + user.profilePicture
-                : PF + "userimg.jpg"
-            }
-            alt="img"
-          ></img>
+          <Link to={`/profile/${user.username}`}>
+            <img
+              className="rightbarProfileImg"
+              src={
+                user.profilePicture
+                  ? PF + user.profilePicture
+                  : PF + "userimg.jpg"
+              }
+              alt="img"
+            ></img>
+          </Link>
           <span className="rightbarOnline"></span>
         </div>
         <span className="rightbarUsername">{user.username}</span>
