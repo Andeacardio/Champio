478466203c53ffe8498dd59ad8599571e9bb44e2
Andeacardio <107910228+Andeacardio@users.noreply.github.com>
2023-06-11 17:43:16 +0300

    Update Profile.jsx

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./profile.css";
 import Topbar from "../../components/topbar/Topbar";
 import Feed from "../../components/feed/Feed";
@@ -6,11 +6,16 @@ import Rightbar from "../../components/rightbar/Rightbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import axios from "axios";
 import { useParams } from "react-router";
+import { PermMedia, Cancel } from "@mui/icons-material";
+import { AuthContex } from "../../context/AuthContext";
 
 export default function Profile() {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   const [user, setUser] = useState({});
   const username = useParams().username;
+  const [file, setFile] = useState(null);
+  const [files, setFiles] = useState(null);
+  const { user: currentUser } = useContext(AuthContex);
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -21,6 +26,70 @@ export default function Profile() {
     };
     fetchUser();
   }, [username]);
+
+  const submitHandl = async (e) => {
+    e.preventDefault();
+    const newPhoto = {
+      userId: user._id,
+    };
+    if (file) {
+      const data = new FormData();
+      const fileName = file.name;
+      data.append("file", file);
+      data.append("name", fileName);
+      newPhoto.profilePicture = fileName;
+
+      try {
+        await axios.post(
+          "https://socialmedia-rest-api.onrender.com/api/upload",
+          data
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    try {
+      await axios.put(
+        "https://socialmedia-rest-api.onrender.com/api/users/" + user._id,
+        newPhoto
+      );
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const submitHand = async (e) => {
+    e.preventDefault();
+    const newPhoto = {
+      userId: user._id,
+    };
+    if (files) {
+      const data = new FormData();
+      const fileName = files.name;
+      data.append("file", files);
+      data.append("name", fileName);
+      newPhoto.coverPicture = fileName;
+
+      try {
+        await axios.post(
+          "https://socialmedia-rest-api.onrender.com/api/upload",
+          data
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    try {
+      await axios.put(
+        "https://socialmedia-rest-api.onrender.com/api/users/" + user._id,
+        newPhoto
+      );
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
       <>
@@ -37,6 +106,24 @@ export default function Profile() {
                   }
                   alt="profile img"
                 ></img>
+                <div className="UserPhotoAdd">
+                  <label htmlFor="filess" className="shareOption">
+                    {username === currentUser.username && (
+                      <span className="shareOptionUserPhoto forCoverPicture">
+                        +
+                      </span>
+                    )}
+                    <input
+                      type="file"
+                      id="filess"
+                      style={{ display: "none" }}
+                      accept=".png,.jpeg,.jpg"
+                      onChange={(e) => {
+                        setFiles(e.target.files[0]);
+                      }}
+                    />
+                  </label>
+                </div>
                 <img
                   className="profileUserImg"
                   src={
@@ -47,6 +134,57 @@ export default function Profile() {
                   alt="user img"
                 ></img>
               </div>
+              <div className="UserPhotoAdd">
+                <label htmlFor="files" className="shareOption">
+                  {username === currentUser.username && (
+                    <span className="shareOptionUserPhoto">+</span>
+                  )}
+                  <input
+                    type="file"
+                    id="files"
+                    style={{ display: "none" }}
+                    accept=".png,.jpeg,.jpg"
+                    onChange={(e) => {
+                      setFile(e.target.files[0]);
+                    }}
+                  />
+                </label>
+              </div>
+              <div>
+                {file && (
+                  <div className="shareImgContainer shareImgProfile">
+                    <img
+                      className="shareImg shareImgIcon"
+                      src={URL.createObjectURL(file)}
+                      alt=""
+                    />
+                    <Cancel
+                      className="shareCancelImg CanselPhoto"
+                      onClick={() => setFile(null)}
+                    />
+                    <button className="uploadButton" onClick={submitHandl}>
+                      upload
+                    </button>
+                  </div>
+                )}
+                {files && (
+                  <div className="shareImgContainer shareImgProfile filesContainer">
+                    <img
+                      className="shareImg shareImgIcon filesImg"
+                      src={URL.createObjectURL(files)}
+                      alt=""
+                    />
+                    <Cancel
+                      className="shareCancelImg CanselPhoto"
+                      onClick={() => setFiles(null)}
+                    />
+                    <button className="uploadButton" onClick={submitHand}>
+                      upload
+                    </button>
+                  </div>
+                )}
+              </div>
+
               <div className="profileInfo">
                 <h4 className="profileInfoName">{user.username}</h4>
                 <p className="profileInfoDesc">{user.desc}</p>
